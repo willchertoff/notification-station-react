d2202a48ad743418eeebdcb25b6819658eb51ac6
Will Chertoff <willchertoff@gmail.com>
2017-01-18 16:06:03 +0100

    chore(index): add public API to index.js

---
+++ src/index.js
@@ -0,0 +1,7 @@
+import NotificationStation from './NotificationStation';
+import NotificationsContainer from './NotificationsContainer';
+import Notification from './Notification';
+
+export NotificationStation;
+export NotificationsContainer;
+export Notification;
